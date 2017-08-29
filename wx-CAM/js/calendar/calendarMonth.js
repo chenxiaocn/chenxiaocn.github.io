2a6fdfbb957b20af207775c30903b9fcf9a79d66
chenxiao <375744914@qq.com>
2017-08-29 11:12:44 +0800

    上拉加载下拉刷新

--- wx-CAM/js/calendar/calendarMonth.js
+++ wx-CAM/js/calendar/calendarMonth.js
@@ -1,20 +1,41 @@
 mui.ready(function() {
 	getCalendarList();
+	
+	//上拉加载，下拉刷新
+	mui.init({
+		pullRefresh: {
+			container: '#pullrefresh',
+			down: {
+				callback: pulldownRefresh
+			},
+			up: {
+				contentrefresh: '正在加载...',
+				callback: pullupRefresh
+			}
+		}
+	});
 });
+
 
 function getCalendarList() {
 	var list='',calWrap='';
-	for(var i=2017;i>2014;i--){
-		var monthList=getMonthList();
-		calWrap='<div class="cal-wrap">'
+	for(var i=2017-2;i<=2017;i++){
+		calWrap=getCalWrapCell(i);
+		list+=calWrap;
+	}
+	$('#calendar').append(list);
+}
+
+function getCalWrapCell(i){
+	var calWrapCell='';
+	var monthList=getMonthList();
+		calWrapCell='<div class="cal-wrap" data-id='+i+'>'
 					+'<h2>'+i+'年'+'</h2>'
 	                +'<ul class="mui-row cal-ul">'	
 	                + monthList
 					+'</ul>'
 					+'</div>';
-		list+=calWrap;
-	}
-	$('#calendar').append(list);
+		return calWrapCell;
 }
 
 function getMonthList(){
@@ -26,4 +47,33 @@ function getMonthList(){
 		list+=item;
 	}
 	return list;
+}
+
+
+/**
+ * 下拉刷新具体业务实现
+ */
+function pulldownRefresh() {
+	setTimeout(function() {
+		var firstChild=$($(".cal-wrap")[0]);
+		var firstChildId=firstChild.attr('data-id');
+		var downCell=getCalWrapCell(firstChildId-1);
+		$('#calendar').prepend(downCell);//下拉刷新，新纪录插到最前面；
+		mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); 
+	}, 300);
+}
+
+/**
+ * 上拉加载具体业务实现
+ */
+function pullupRefresh() {
+	var count = 0;
+	setTimeout(function() {
+		mui('#pullrefresh').pullRefresh().endPullupToRefresh((++count > 2)); //参数为true代表没有更多数据了。
+		var length=$(".cal-wrap").length;
+		var lastChild=$($(".cal-wrap")[length-1]);
+		var lastChildId=lastChild.attr('data-id');
+		var upCell=getCalWrapCell(parseInt(lastChildId)+1);
+		$('#calendar').append(upCell);
+	}, 300);
 }
