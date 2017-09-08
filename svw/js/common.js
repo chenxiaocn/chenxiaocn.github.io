0957e8a18cff46c4a0ca9d531822f49b2e8d551b
chenxiao <375744914@qq.com>
2017-09-08 16:34:21 +0800

    优化

--- svw/js/common.js
+++ svw/js/common.js
@@ -1,20 +1,3 @@
-///////////全选或无线开关//////////
-mui.init({
-	swipeBack: true //启用右滑关闭功能
-});
-mui('.mui-switch').each(function() { //循环所有toggle
-	var switchName = localStorage.getItem('switchName');
-	//toggle.classList.contains('mui-active') 可识别该toggle的开关状态
-	this.parentNode.querySelector('span').innerText = switchName;
-	/**
-	 * toggle 事件监听
-	 */
-	//		this.addEventListener('toggle', function(event) {
-	//			//event.detail.isActive 可直接获取当前状态
-	////			this.parentNode.querySelector('span').innerText = '状态';
-	//		});
-});
-
 ///////////页面跳转//////////
 mui('.mui-content').on('tap', '.go-next', function(e) {
 	var thisValue = $(this).attr('data-value');
